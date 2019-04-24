d92c5d992d4b9a154de5f3982197141a78b7c9a0
renmfe <499809015@qq.com>
2019-04-24 16:07:49 +0800

    开发环境

--- db/DBConfig.js
+++ db/DBConfig.js
@@ -2,7 +2,7 @@ module.exports = {
 
     host: '127.0.0.1',
     user: 'root',
-    password: '1234abcd!', //生产
+    password: '1234abcd.',
     database: 'express', // 前面建的user表位于这个数据库中 
     // port: 3306
     // }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '1234abcd!', //生产
+    password: '1234abcd.',
     database: 'express'
 });
 
